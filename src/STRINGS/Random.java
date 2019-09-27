2f2ee02a4ea60cb75f7ef638d2763d7aa3606ea4
ashishgamut <askashish.kr@gmail.com>
2019-09-27 21:32:05 +0530

    ashish in develop branch

--- src/STRINGS/Random.java
+++ src/STRINGS/Random.java
@@ -22,7 +22,8 @@ System.out.println(cal.get(Calendar.DAY_OF_MONTH));
 
 public static void gitcode()
 {
-	
+	System.out.println("things are great");
+	System.out.println("things are good");
 }
 	
 	
